8731972a7ddd0f123d62afab137dd8dec95308c1
FrontEdd <edgar@urbaner.com>
2022-08-03 23:56:45 -0500

    Implementamos interface de busqueda para mostrar respuesta con una lista recorrida mediante map y agregamos navBar para mostrar respuesta del input y button para regresar al search

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,18 +1,64 @@
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+import {
+	Box,
+	AppBar,
+	Toolbar,
+	Typography,
+	Button,
+	Container,
+} from "@mui/material";
+
 import Services from "../../Services";
+import { CardMovie } from "../../components";
 
 const SearchResults = () => {
-    const { searchText } = useParams();
+    const [movies, setMovies] = useState([]);
+	const { searchText } = useParams();
+	const history = useNavigate();
 
     async function getSearchResults() {
-        const data = await Services.searchByText(searchText);
-        console.log(data.Search);
-    }
+		const data = await Services.searchByText(searchText);
+		setMovies(data.Search);
+		console.log(data.Search);
+	}
 
-    useEffect(() => {
-        getSearchResults();
-    }, []);
+	useEffect(() => {
+		getSearchResults();
+	}, []);
+
+	function backButton() {
+		history("/search");
+	}
+    return (
+        <Box>
+            <AppBar position="static">
+				<Toolbar>
+					<Typography
+						variant="h6"
+						component="div"
+						sx={{ flexGrow: 1 }}
+					>
+						Search Results: "{searchText}"
+					</Typography>
+					<Button
+						variant="outlined"
+						color="error"
+						onClick={backButton}
+					>
+						Back
+					</Button>
+				</Toolbar>
+			</AppBar>
+			<Container>
+				{movies.length > 0 &&
+					movies.map((movie, index) => (
+						<CardMovie movie={movie} key={index} />
+					))}
+			</Container>
+        </Box>
+    );
 };
 
 export default SearchResults;
