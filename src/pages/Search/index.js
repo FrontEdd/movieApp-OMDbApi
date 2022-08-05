08186f96234df60871c81781ec5390ca4d9ee5ce
FrontEdd <edgar@urbaner.com>
2022-08-04 20:25:38 -0500

    Implementamos en remplazo de main un login y adicional agregamos navBar para la pg /search

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
-import { Box, Button, Card, CardContent, Container, Grid, Stack, TextField, Typography } from "@mui/material";
+import LogoutIcon from '@mui/icons-material/Logout';
+import { AppBar, Box, Button, Card, CardContent, Container, Grid, IconButton, Stack, TextField, Toolbar, Typography } from "@mui/material";
 
 
 const Search = () => {
@@ -20,41 +20,59 @@ const Search = () => {
     }
 
     return (
-        <Container maxWidth="sm">
-            <Card sx={{marginTop: 20}}>
-                <CardContent>
-                    <Grid>
-                        <Box>
-                            <Stack sx={{textAlign: 'center'}}>
-                                <Typography
-                                    
-                                    variant="h2"
-                                >
-                                    OMDb Search
-                                </Typography>
-                            </Stack>
-                        </Box>
-                        <Box>
-                            <Stack direction="row" spacing={1}>
-                                <TextField
-                                    fullWidth
-                                    label="Movie or tv Show"
-                                    value={searchText}
-                                    onChange={searchInput}
-                                />
-                                <Button
-                                    fullWidth
-                                    variant="contained"
-                                    onClick={searchButton}
-                                >
-                                    Search
-                                </Button>
-                            </Stack>
-                        </Box>
-                    </Grid>
-                </CardContent>
-            </Card>
-        </Container>
+        <>
+            <Box>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography
+                            variant="h6"
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            Edgar Razuri
+                        </Typography>
+                        <IconButton color="secondary"> 
+                            <LogoutIcon/>
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <Container maxWidth="sm">
+                <Card sx={{marginTop: 20}}>
+                    <CardContent>
+                        <Grid>
+                            <Box>
+                                <Stack sx={{textAlign: 'center'}}>
+                                    <Typography
+                                        
+                                        variant="h2"
+                                    >
+                                        OMDb Search
+                                    </Typography>
+                                </Stack>
+                            </Box>
+                            <Box>
+                                <Stack direction="row" spacing={1}>
+                                    <TextField
+                                        fullWidth
+                                        label="Movie or tv Show"
+                                        value={searchText}
+                                        onChange={searchInput}
+                                    />
+                                    <Button
+                                        fullWidth
+                                        variant="contained"
+                                        onClick={searchButton}
+                                    >
+                                        Search
+                                    </Button>
+                                </Stack>
+                            </Box>
+                        </Grid>
+                    </CardContent>
+                </Card>
+            </Container>
+        </>
     )
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
-export { default as MainView } from "./Main";
+// export { default as MainView } from "./Main";
+export { default as LoginView } from "./Login";
 export { default as SearchView } from "./Search";
 export { default as SearchResultsView } from "./SearchResults";
