e1ed02a5a5fe4e7754c94ae4131d7409da8a43af
FrontEdd <edgar@urbaner.com>
2022-08-08 21:26:59 -0500

    Implementamos boton de compra en CardMovie

--- src/components/DrawerCart/index.js
+++ src/components/DrawerCart/index.js
@@ -1,19 +1,33 @@
-import { Box, IconButton, Badge } from "@mui/material";
+import { useState } from "react";
+import { Box, IconButton, Badge, Drawer, Typography } from "@mui/material";
 import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
 
 
 const DrawerCart = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(!open);
+
   return (
     <Box>
       <IconButton
-          sx={{
+        onClick={handleOpen}
+        sx={{
           color: "#fff",
-          }}
+        }}
       >
-          <Badge badgeContent={4} color="secondary">
+        <Badge badgeContent={4} color="secondary">
           <ShoppingCartRoundedIcon />
-          </Badge>
+        </Badge>
       </IconButton>
+      <Drawer anchor="right" open={open} onClose={handleOpen}>
+        <Box p={5} mt={6}>
+          <Typography variant="h6">Resume cart</Typography>
+          <Typography variant="body1">
+            Resume of my movie and tv shows
+          </Typography>
+        </Box>
+      </Drawer>
     </Box>
   )
 };
